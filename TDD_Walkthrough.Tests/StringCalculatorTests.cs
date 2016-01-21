877d4631576c1fdec12a43d4def4675e2c839e86
BarryBurke <barry.burke@gmail.com>
2016-01-21 18:43:19 +0000

    Initial Commit

---
+++ TDD_Walkthrough.Tests/StringCalculatorTests.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD_Walkthrough.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {      
+        [TestMethod]
+        public void Add_WhenAnEmptyStringIsUsedThenZeroIsReturned()
+        {
+            StringCalculator sc = new StringCalculator();
+            string emptyParameter = string.Empty;
+
+            var result = sc.Add(emptyParameter);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenOneNumberIsUsedThenThatNumberIsReturned()
+        {
+            StringCalculator sc = new StringCalculator();
+            string oneNumberParameter = "1";
+
+            var result = sc.Add(oneNumberParameter);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenTwoNumbersAreUsedThenTheSumOfTheNumbersIsReturned()
+        {
+            StringCalculator sc = new StringCalculator();
+            string twoNumberParameter = "1,2";
+
+            var result = sc.Add(twoNumberParameter);
+            Assert.AreEqual(1+2, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenAnyNumbersofNumbersAreUsedThenTheSumOfTheNumbersIsReturned()
+        {
+            StringCalculator sc = new StringCalculator();
+            string twoNumberParameter = "1,2,3,4,5";
+
+            var result = sc.Add(twoNumberParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenANewLineCharacterIsUsedThenItIsTreatedAsADelimiterAndTheSumOfTheNumbersIsReturned()
+        {
+            StringCalculator sc = new StringCalculator();
+            string twoNumberParameter = "1,2\n3,4,5";
+
+            var result = sc.Add(twoNumberParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenADelimiterParameterIsSpecfiedThenItIsUsedToSeparateTheNumbers()
+        {
+            StringCalculator sc = new StringCalculator();
+            string twoNumberParameter = "1;2;3;4;5";
+            string[] delimiters = new string[] { ";" };
+
+            var result = sc.Add(twoNumberParameter, delimiters);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenASingleCharacterDelimiterIsSpecfiedInTheNumberParameterThenItIsUsedToSeparateTheNumbers()
+        {
+            StringCalculator sc = new StringCalculator();
+            string numbersParameter = "//;\n1;2;3;4;5";
+
+            var result = sc.Add(numbersParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenAMultipleCharacterDelimiterIsSpecfiedInTheNumberParameterThenItIsUsedToSeparateTheNumbers()
+        {
+            StringCalculator sc = new StringCalculator();
+            string numbersParameter = "//[---]\n1---2---3---4---5";
+
+            var result = sc.Add(numbersParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenManySingleCharacterDelimitersAreSpecfiedInTheNumberParameterThenTheyAreUsedToSeparateTheNumbers()
+        {
+            StringCalculator sc = new StringCalculator();
+            string numbersParameter = "//[-][%]\n1-2-3%4%5";
+
+            var result = sc.Add(numbersParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        public void Add_WhenManyMultipleCharacterDelimitersAreSpecfiedInTheNumberParameterThenTheyAreUsedToSeparateTheNumbers()
+        {
+            StringCalculator sc = new StringCalculator();
+            string numbersParameter = "//[--][%%]\n1--2--3%%4%%5";
+
+            var result = sc.Add(numbersParameter);
+            Assert.AreEqual(1 + 2 + 3 + 4 + 5, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Add_WhenAnyNonNumberIsUsedThenAnExceptionIsThrown()
+        {
+            StringCalculator sc = new StringCalculator();
+            string invalidNumberParameter = "1,X";
+
+            sc.Add(invalidNumberParameter);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Add_WhenAnyNegativeNumberIsUsedThenAnExceptionIsThrown()
+        {
+            StringCalculator sc = new StringCalculator();
+            string negativeNumbersParameter = "1,-2,3,4,-5";
+
+            sc.Add(negativeNumbersParameter);
+        }
+
+        [TestMethod]
+        public void Add_WhenAnyNegativeNumberIsUsedThenAnNegativeNumbersAreReturnedInTheThrownException()
+        {
+            Exception exception = null;
+            try
+            {
+                StringCalculator sc = new StringCalculator();
+                string negativeNumbersParameter = "1,-2,3,4,-5";
+
+                sc.Add(negativeNumbersParameter);
+            }
+            catch(Exception e)
+            {
+                exception = e;
+            }
+            Assert.IsNotNull(exception);
+            Assert.AreEqual("Negatives not allowed: [-2,-5]", exception.Message);
+        }
+
+        [TestMethod]
+        public void Add_WhenAnyNumbersGreaterThan1000AreUsedThenTheseAreExcludedFromTheSum()
+        {
+            StringCalculator sc = new StringCalculator();
+            string numbersParameter = "1,2,3000,4,5";
+
+            var result = sc.Add(numbersParameter);
+            Assert.AreEqual(1 + 2 + 4 + 5, result);
+        }
+
+        //[TestMethod]
+        //[ExpectedException(typeof(Exception))]
+        //public void Add_WhenMoreThanTwoNumberAreUsedThenAnExceptionIsThrown()
+        //{
+        //    StringCalculator sc = new StringCalculator();
+        //    string manyNumbersParameter = "1,2,4";
+
+        //    sc.Add(manyNumbersParameter);
+        //}
+
+    }
+}
---
+++ TDD_Walkthrough/StringCalculator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Walkthrough
+{
+    public class StringCalculator
+    {
+        private const string DELIMITER_IDENTIFIER = "//";
+        private const string MULTIPLE_CHARACTER_STARTING_DELIMITER_IDENTIFIER = "[";
+        private const string MULTIPLE_CHARACTER_ENDING_DELIMITER_IDENTIFIER = "]";
+     
+        public List<string> Delimiters { get; private set; }
+
+        public StringCalculator()
+        {
+            Delimiters = new List<string>();
+            Delimiters.Add(",");
+            Delimiters.Add("\n");
+        }
+     
+        public int Add(string numbers, string[] delimiters)
+        {
+            Delimiters.AddRange(delimiters.ToList());
+
+            return Add(numbers);
+        }
+
+        public int Add(string numbers)
+        {
+            int result = 0;
+
+            if (numbers.StartsWith(DELIMITER_IDENTIFIER))
+            {
+                var delimiterIndex = numbers.IndexOf("\n");
+                string newDelimiters = numbers.Substring(DELIMITER_IDENTIFIER.Length, delimiterIndex - 2);
+              
+                if(newDelimiters.Contains(MULTIPLE_CHARACTER_STARTING_DELIMITER_IDENTIFIER) && newDelimiters.Contains(MULTIPLE_CHARACTER_ENDING_DELIMITER_IDENTIFIER))
+                {
+                    string[] multipleDelimiters = newDelimiters.Split(MULTIPLE_CHARACTER_ENDING_DELIMITER_IDENTIFIER.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach(var md in multipleDelimiters)
+                    {                        
+                        Delimiters.Add(md.TrimStart(MULTIPLE_CHARACTER_STARTING_DELIMITER_IDENTIFIER.ToCharArray()));
+                    }
+                }
+                else
+                {
+                    Delimiters.Add(newDelimiters);
+                }
+                               
+                numbers = numbers.Substring(delimiterIndex + 1); // remove delimiter from string
+            }
+
+            string[] nums = numbers.Split(Delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+           
+            List<int> negativeNumbers = new List<int>();
+            foreach(var n in nums)
+            {
+                if (string.IsNullOrEmpty(n) == false)
+                {
+                    int number = int.Parse(n);
+                    if(number < 0)
+                    {
+                        negativeNumbers.Add(number);
+                    }
+                    else if (number <= 1000)
+                    {
+                        result += number;
+                    } 
+                }
+            }
+
+            if (negativeNumbers.Count() > 0)
+            {
+                string message = "Negatives not allowed: [";
+
+                foreach (var n in negativeNumbers)
+                    message += n.ToString() + ",";
+
+                message = message.TrimEnd(',');
+                message += "]";
+
+                throw new Exception(message);
+            }
+
+            return result;
+        }
+    }
+}
